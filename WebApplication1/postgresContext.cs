2573976a50575c2490f5732b411e4309c586fde9
Aleksey Grigoryev <drummaprod@gmail.com>
2019-02-04 13:34:13 +0400

    trying to connect postgre

---
+++ WebApplication1/postgresContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication1
+{
+    public partial class postgresContext : DbContext
+    {
+        public postgresContext()
+        {
+        }
+
+        public postgresContext(DbContextOptions<postgresContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=root");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasPostgresExtension("adminpack")
+                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+        }
+    }
+}
