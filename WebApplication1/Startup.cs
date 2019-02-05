8bef6b8b05f50a88130f83430295010980a82cb3
Aleksey Grigoryev <drummaprod@gmail.com>
2019-02-05 18:39:46 +0400

    some changes to connection string / change way of connection

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -36,7 +36,7 @@ namespace WebApplication1
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
